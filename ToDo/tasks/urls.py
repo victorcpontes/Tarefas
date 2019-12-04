613263e00a6e06c1aa6897ed31fdbf555cc57228
victorcpontes <58314798+victorcpontes@users.noreply.github.com>
2019-12-04 09:58:44 -0300

    Add files via upload

---
+++ ToDo/tasks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.taskList, name='task-list'),
+    path('tasks/<int:id>', views.taskView, name="task-view"),
+    path('newtask/', views.newTask, name="new-task"),
+    path('edit/<int:id>', views.editTask, name="edit-task"),
+    path('changestatus/<int:id>', views.changeStatus, name="change-status"),
+    path('delete/<int:id>', views.deleteTask, name="delete-task"),
+]
